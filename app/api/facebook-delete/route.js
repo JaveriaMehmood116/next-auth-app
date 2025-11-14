abf55c40c0c1f0c16d518eb720f5f47e6fac461c
Javeria Mehmood <javeriamehmood2004@gmail.com>
2025-11-14 23:27:03 +0500

    final pls

--- app/api/facebook-delete/route.js
+++ app/api/facebook-delete/route.js
@@ -2,6 +2,7 @@ export async function GET() {
   return Response.json({
     success: true,
     message: "User data deletion instructions.",
-    instructions_url: "https://next-auth-app-git-main-javeria-mehmoods-projects.vercel.app/facebook-data-deletion"
+    instructions_url: "https://next-auth-app-kohl.vercel.app/facebook-data-deletion"
   });
 }
+
